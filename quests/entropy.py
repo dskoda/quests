97e0959c232401a45a8b342401755ec0839ebd09
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2023-09-28 08:37:32 -0700

    implement double batch for delta entropy computation

--- quests/entropy.py
+++ quests/entropy.py
@@ -9,6 +9,7 @@ from .matrix import norm
 
 DEFAULT_BANDWIDTH = 0.015
 DEFAULT_BATCH = 2000
+DEFAULT_REF_BATCH = 100000
 
 
 @nb.njit(fastmath=True)
@@ -38,12 +39,13 @@ def perfect_entropy(
 
 @nb.njit(fastmath=True, parallel=True)
 def delta_entropy(
+    x: np.ndarray,
     ref: np.ndarray,
-    test: np.ndarray,
     h: float = DEFAULT_BANDWIDTH,
     batch_size: int = DEFAULT_BATCH,
+    ref_batch_size: int = DEFAULT_REF_BATCH,
 ):
-    """Computes the delta entropy of a dataset `test` using the dataset
+    """Computes the delta entropy of a dataset `x` using the dataset
         `ref` as a reference. The function uses a batch distance
         calculation. This is necessary because the full distance matrix
         often does not fit in the memory for a big dataset. This function
@@ -51,30 +53,48 @@ def delta_entropy(
         does not approximate the results.
 
     Arguments:
-        ref (np.ndarray): an (N, d) matrix with the descriptors
-        test (np.ndarray): an (M, d) matrix with the descriptors
+        x (np.ndarray): an (N, d) matrix with the descriptors
+        ref (np.ndarray): an (M, d) matrix with the descriptors
         h (int): bandwidth for the Gaussian kernel
         batch_size (int): maximum batch size to consider when
             performing a distance calculation.
+        ref_batch_size (int): maximum batch size to consider when
+            looping over columns
 
     Returns:
         entropies (np.ndarray): a (M,) vector containing all entropies of
-            `test` computed with respect to `ref`.
+            `x` computed with respect to `ref`.
     """
-    N = test.shape[0]
-    max_step = math.ceil(N / batch_size)
+    N = x.shape[0]
+    max_step_x = math.ceil(N / batch_size)
+
+    M = ref.shape[0]
+    max_step_ref = math.ceil(M / ref_batch_size)
 
     norm_ref = norm(ref)
-    norm_test = norm(test)
-    entropies = np.empty(N, dtype=test.dtype)
+    norm_x = norm(x)
+    entropies = np.empty(N, dtype=x.dtype)
 
-    for step in nb.prange(0, max_step):
-        i = step * batch_size
+    # loops over rows and columns to compute the
+    # distance matrix without keeping it entirely
+    # in the memory
+    for step_x in nb.prange(0, max_step_x):
+        i = step_x * batch_size
         imax = min(i + batch_size, N)
-        batch = test[i:imax]
-        batch_norm = norm_test[i:imax]
+        n_rows = imax - i
+        x_batch = x[i:imax]
+        x_batch_norm = norm_x[i:imax]
 
-        d = cdist(batch, ref, batch_norm, norm_ref)
+        # loops over all columns in batches to prevent memory overflow
+        d = np.empty((n_rows, M))
+        for step_ref in range(0, max_step_ref):
+            j = step_ref * ref_batch_size
+            jmax = min(j + ref_batch_size, M)
+            ref_batch = ref[j:jmax]
+            ref_batch_norm = norm_ref[j:jmax]
+
+            _dists = cdist(x_batch, ref_batch, x_batch_norm, ref_batch_norm)
+            d[:, j:jmax] = _dists
 
         # computation of the entropy
         z = d / h
