e1e1b3e48c1a0d60351fdcb2efd9c24f5b199b4a
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2023-11-28 15:27:37 -0800

    mutual information

--- quests/cli/quests.py
+++ quests/cli/quests.py
@@ -3,6 +3,7 @@ import click
 from quests.cli.make_descriptors import make_descriptors
 from quests.cli.entropy import entropy
 from quests.cli.entropy_sampler import entropy_sampler
+from quests.cli.mutual_info import mutual_info
 
 
 class QuestsGroup(click.Group):
@@ -17,6 +18,7 @@ def quests():
 quests.add_command(entropy)
 quests.add_command(entropy_sampler)
 quests.add_command(make_descriptors)
+quests.add_command(mutual_info)
 
 
 if __name__ == "__main__":
