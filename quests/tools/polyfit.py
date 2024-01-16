6757fdc2ac2b0ced44e5ce3e33a1f90fc3d62a31
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2024-01-16 07:40:37 -0800

    plotting and polynomial fit tools

---
+++ quests/tools/polyfit.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+
+class PolynomialRegressor2D:
+    """Create a regressor that fits a 2D polynomial using the provided
+        data points.
+    """
+    def __init__(self, degree: int, x_train: np.ndarray, y_train: np.ndarray):
+        """Initializes the 2D polynomial regressor using some training data.
+
+        Arguments:
+            degree (int): degree of the polynomial
+            x_train (np.ndarray): (N, 2) data for the coordinates of
+                input points
+            y_train (np.ndarray): (N, ) data for the coordinates of
+                the output prediction.
+        """
+        self.degree = degree
+        poly_features = PolynomialFeatures(degree)
+        self.x_poly = poly_features.fit_transform(x_train)
+        self.model = LinearRegression().fit(self.x_poly, y_train)
+        self.y_train = y_train
+
+    def __call__(self, x):
+        poly_features = PolynomialFeatures(self.degree)
+        x_poly = poly_features.fit_transform(x)
+        return self.model.predict(x_poly)
+
+    def training_error(self):
+        predictions = self.model.predict(self.x_poly)
+        return np.mean((self.y_train - predictions) ** 2)
