a7a7eb244fb9f4c4551ed22e400a2b224d88ba4c
Daniel Schwalbe Koda <schwalbekoda1@llnl.gov>
2024-01-25 00:15:31 -0800

    compute dH command

---
+++ quests/cli/compute_dH.py
@@ -0,0 +1,129 @@
+import gc
+import json
+import time
+
+import click
+import numba as nb
+import numpy as np
+from ase.io import read
+
+from .log import format_time
+from .log import logger
+from quests.descriptor import DEFAULT_CUTOFF
+from quests.descriptor import DEFAULT_K
+from quests.descriptor import get_descriptors
+from quests.entropy import DEFAULT_BANDWIDTH
+from quests.entropy import DEFAULT_BATCH
+from quests.entropy import delta_entropy
+from quests.tools.time import Timer
+
+
+def descriptors_from_file(file, k, cutoff):
+    if file.endswith(".npz"):
+        with open(file, "rb") as f:
+            return np.load(f)
+
+    logger(f"Loading and creating descriptors for file {file}")
+    dset = read(file, index=":")
+
+    with Timer() as t:
+        x = get_descriptors(dset, k=k, cutoff=cutoff)
+    descriptor_time = t.time
+    logger(f"Descriptors built in: {format_time(descriptor_time)}")
+    logger(f"Descriptors shape: {x.shape}")
+
+    return x
+
+
+@click.command("dH")
+@click.argument("test", required=1)
+@click.argument("reference", required=1)
+@click.option(
+    "-c",
+    "--cutoff",
+    type=float,
+    default=DEFAULT_CUTOFF,
+    help=f"Cutoff (in Å) for computing the neighbor list (default: {DEFAULT_CUTOFF:.1f})",
+)
+@click.option(
+    "-k",
+    "--nbrs",
+    type=int,
+    default=DEFAULT_K,
+    help=f"Number of neighbors when creating the descriptor (default: {DEFAULT_K})",
+)
+@click.option(
+    "-b",
+    "--bandwidth",
+    type=float,
+    default=DEFAULT_BANDWIDTH,
+    help=f"Bandwidth when computing the kernel (default: {DEFAULT_BANDWIDTH})",
+)
+@click.option(
+    "-j",
+    "--jobs",
+    type=int,
+    default=None,
+    help="Number of jobs to distribute the calculation in (default: all)",
+)
+@click.option(
+    "--batch_size",
+    type=int,
+    default=DEFAULT_BATCH,
+    help=f"Size of the batches when computing the distances (default: {DEFAULT_BATCH})",
+)
+@click.option(
+    "-o",
+    "--output",
+    type=str,
+    default=None,
+    help="path to the json file that will contain the output\
+            (default: no output produced)",
+)
+@click.option(
+    "--overwrite",
+    is_flag=True,
+    default=False,
+    help="If True, overwrite the output file",
+)
+def dH(
+    test,
+    reference,
+    cutoff,
+    nbrs,
+    bandwidth,
+    jobs,
+    batch_size,
+    output,
+    overwrite,
+):
+    if output is not None and os.path.exists(output) and not overwrite:
+        logger(f"Output file {output} exists. Aborting...")
+        sys.exit(0)
+
+    if jobs is not None:
+        nb.set_num_threads(jobs)
+
+    x = descriptors_from_file(test, nbrs, cutoff)
+    ref = descriptors_from_file(reference, nbrs, cutoff)
+
+    with Timer() as t:
+        delta = delta_entropy(x, ref, h=bandwidth, batch_size=batch_size)
+    entropy_time = t.time
+    logger(f"dH computed in: {format_time(entropy_time)}")
+
+    if output is not None:
+        results = {
+            "reference_file": reference,
+            "test_file": test,
+            "test_envs": x.shape[0],
+            "ref_envs": ref.shape[0],
+            "k": nbrs,
+            "cutoff": cutoff,
+            "bandwidth": bandwidth,
+            "jobs": jobs,
+            "delta_entropy": list(delta),
+        }
+
+        with open(output, "w") as f:
+            json.dump(results, f, indent=4)
--- quests/cli/quests.py
+++ quests/cli/quests.py
@@ -4,6 +4,7 @@ from quests.cli.make_descriptors import make_descriptors
 from quests.cli.entropy import entropy
 from quests.cli.entropy_sampler import entropy_sampler
 from quests.cli.mutual_info import mutual_info
+from quests.cli.compute_dH import dH
 
 
 class QuestsGroup(click.Group):
@@ -19,6 +20,7 @@ quests.add_command(entropy)
 quests.add_command(entropy_sampler)
 quests.add_command(make_descriptors)
 quests.add_command(mutual_info)
+quests.add_command(dH)
 
 
 if __name__ == "__main__":
