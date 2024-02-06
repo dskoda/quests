fix bugs with entropy, overflow, and segfaults

quests/entropy.py
@@ -14,12 +14,10 @@ DEFAULT_BATCH = 20000
 DEFAULT_UQ_NBRS = 3
 
 
-@nb.njit(fastmath=True, cache=True)
 def perfect_entropy(
     x: np.ndarray,
     h: float = DEFAULT_BANDWIDTH,
     batch_size: int = DEFAULT_BATCH,
-    eps: float = 1e-10
 ):
     """Computes the perfect entropy of a dataset using a batch distance
         calculation. This is necessary because the full distance matrix
@@ -40,22 +38,18 @@ def perfect_entropy(
     """
     N = x.shape[0]
     p_x = kernel_sum(x, x, h=h, batch_size=batch_size)
-    p_x = p_x + eps
 
     # normalizes the p(x) prior to the log for numerical stability
-    for j in range(N):
-        p_x[j] = math.log(p_x[j] / N)
+    p_x = np.log(p_x / N)
 
     return -np.mean(p_x)
 
 
-@nb.njit(fastmath=True, cache=True)
 def delta_entropy(
     x: np.ndarray,
     y: np.ndarray,
     h: float = DEFAULT_BANDWIDTH,
     batch_size: int = DEFAULT_BATCH,
-    eps: float = 1e-10
 ):
     """Computes the differential entropy of a dataset `x` using the dataset
         `y` as reference. This function can be SLOW, despite the optimization
@@ -75,12 +69,7 @@ def delta_entropy(
     """
     N = x.shape[0]
     p_x = kernel_sum(x, y, h=h, batch_size=batch_size)
-    p_x = p_x + eps
-
-    for j in range(N):
-        p_x[j] = -math.log(p_x[j])
-
-    return p_x
+    return -np.log(p_x)
 
 
 @nb.njit(fastmath=True, parallel=True, cache=True)
@@ -125,14 +114,14 @@ def kernel_sum(
     # in the memory
     for step_x in nb.prange(0, max_step_x):
         i = step_x * batch_size
-        imax = min(i + batch_size, M - 1)
+        imax = min(i + batch_size, M)
         x_batch = x[i:imax]
         x_batch_norm = norm_x[i:imax]
 
         # loops over all columns in batches to prevent memory overflow
         for step_y in range(0, max_step_y):
             j = step_y * batch_size
-            jmax = min(j + batch_size, N - 1)
+            jmax = min(j + batch_size, N)
             y_batch = y[j:jmax]
             y_batch_norm = norm_y[j:jmax]
 