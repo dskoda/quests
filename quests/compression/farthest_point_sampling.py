c995170fa112ee82c8035258a9c04c8ecea400c6
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2024-08-13 15:42:21 -0700

    reformat

--- quests/compression/farthest_point_sampling.py
+++ quests/compression/farthest_point_sampling.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 
 DEFAULT_CUTOFF: float = 5.0
 DEFAULT_K: int = 32
@@ -8,69 +8,67 @@ DEFAULT_BS: int = 10000
 
 
 def find_key(input_dict: dict, target: np.ndarray):
-    
     """Given a dictionary of descriptors, determines the index of the target descriptors
-    
-    Arguments: 
+
+    Arguments:
         input_dict (dictionary): dictionary containing descriptors
         target (np.ndarray): numpy array of descriptor
-        
-    Returns: key (int): original index 
-        
-        
+
+    Returns: key (int): original index
+
+
     """
-    
+
     for key in input_dict:
-        if (target.shape != input_dict[key].shape):
+        if target.shape != input_dict[key].shape:
             continue
         if (target == input_dict[key]).all():
             return key
     return None
-    
+
+
 def farthest_point_sampling(frames, initial_entropies, descriptor_dict):
-    
     """Given the frames and initial entropies, determine the most diverse set of atoms in the set
-    
-    Arguments: 
+
+    Arguments:
         frames (list): descriptors of each of the frames
         initial_entropies (np.ndarray): array with initial entropies of each of the frames
         descriptor_dict (dict): dictionary containing descriptors
-        
-    Returns: indexes (list): list of indexes of the most diverse frames in order 
-        
-        
+
+    Returns: indexes (list): list of indexes of the most diverse frames in order
+
+
     """
-    
+
     indexes = []
-    
-    # generate first value 
-    
+
+    # generate first value
+
     data = frames[initial_entropies.argmax()]
     indexes.append(initial_entropies.argmax())
     frames.pop(initial_entropies.argmax())
-    
-    # loop to find order of values 
-    
+
+    # loop to find order of values
+
     for i in range(int(len(frames))):
-        
-        # generate minimum distance matrix 
-        
+
+        # generate minimum distance matrix
+
         min_distance = np.zeros(len(frames))
-        
-        # calculates distance between closest values in sets 
-        
+
+        # calculates distance between closest values in sets
+
         for c in range(len(frames)):
             distance_matrix = np.zeros((len(frames), len(data)))
             for a in range(len(frames)):
                 for b in range(len(data)):
                     distance_matrix[a, b] = np.linalg.norm(data[b] - frames[a])
             min_distance[c] = np.min(distance_matrix)
-            
+
         # appends farthest set
-        
+
         indexes.append(find_key(descriptor_dict, frames[min_distance.argmax()]))
-        data = np.concatenate((data, frames[min_distance.argmax()]), axis = 0)
+        data = np.concatenate((data, frames[min_distance.argmax()]), axis=0)
         frames.pop(min_distance.argmax())
-    
+
     return indexes
-        
--- quests/compression/process.py
+++ quests/compression/process.py
@@ -15,127 +15,155 @@ DEFAULT_H: float = 0.015
 DEFAULT_BS: int = 10000
 
 
-def get_frame_descriptors(dset: List[Atoms], k: int = DEFAULT_K, cutoff: int = DEFAULT_CUTOFF, h: int = DEFAULT_H, batch_size: int = DEFAULT_BS):
-    
+def get_frame_descriptors(
+    dset: List[Atoms],
+    k: int = DEFAULT_K,
+    cutoff: int = DEFAULT_CUTOFF,
+    h: int = DEFAULT_H,
+    batch_size: int = DEFAULT_BS,
+):
     """Gets descriptors for each frame
-    
-    Arguments: 
+
+    Arguments:
         dset (List[Atoms]): dataset for which entropies will be computed
         k (int): number of nearest neighbors to use when computing descriptors.
-        h (int): 
-        batch_size (int): 
+        h (int):
+        batch_size (int):
         cutoff (float): cutoff radius for the weight function.
-        
-    Returns: 
+
+    Returns:
         frames_orig (list): list of the descriptors of each frame (np.ndarray)
-        
+
     """
-    
+
     frames_orig = []
     initial_entropies = []
     for frame in dset:
-        y = get_descriptors([frame], k = k, cutoff = cutoff)
+        y = get_descriptors([frame], k=k, cutoff=cutoff)
         frames_orig.append(y)
-        initial_entropies.append(perfect_entropy(y, h=h, batch_size = batch_size))
+        initial_entropies.append(perfect_entropy(y, h=h, batch_size=batch_size))
     return frames_orig, np.array(initial_entropies)
 
-def compress_dataset(dset: List[Atoms], k: int = DEFAULT_K, cutoff: float = DEFAULT_CUTOFF, h: float = DEFAULT_H, batch_size: int = DEFAULT_BS,
-             compression_value: float = None, c_type: str = 'msc', l: float = None):
-    
+
+def compress_dataset(
+    dset: List[Atoms],
+    k: int = DEFAULT_K,
+    cutoff: float = DEFAULT_CUTOFF,
+    h: float = DEFAULT_H,
+    batch_size: int = DEFAULT_BS,
+    compression_value: float = None,
+    c_type: str = "msc",
+    l: float = None,
+):
     """Gets descriptors for each frame
-    
-    Arguments: 
+
+    Arguments:
         dset (List[Atoms]): dataset for which entropies will be computed
         k (int): number of nearest neighbors to use when computing descriptors.
         cutoff (float)
-        h (float): h value 
-        batch_size (int):  
+        h (float): h value
+        batch_size (int):
         cutoff (float): cutoff radius for the weight function.
-        
-    Returns: 
+
+    Returns:
         frames_orig (list): list of the descriptors of each frame (np.ndarray)
-        
+
     """
-    
-    assert c_type in ['msc', 'msc2', 'fps']
-    
+
+    assert c_type in ["msc", "msc2", "fps"]
+
     # descriptors and initial entropies for each frame in the dataset
     frames, initial_entropies = get_frame_descriptors(dset, k, cutoff, h, batch_size)
-    
-    # dictionary with index & descriptors 
+
+    # dictionary with index & descriptors
     descriptor_dict = {}
     for i in range(len(frames)):
         descriptor_dict[i] = frames[i]
-        
-        
-    if c_type == 'fps':
-        
-        # retrieve indexes 
-        
+
+    if c_type == "fps":
+
+        # retrieve indexes
+
         indexes = farthest_point_sampling(frames, initial_entropies, descriptor_dict)
         if compression_value != None:
-            return dset[indexes[:int(len(dset)*compression_value)]]
+            return dset[indexes[: int(len(dset) * compression_value)]]
         else:
-            
-            # finding optimal compression value 
-            
+
+            # finding optimal compression value
+
             def optimization_function(x):
-                final_data = [frames[i] for i in indexes[:int(len(dset)*x)]]
-                final_data = np.concatenate(final_data, axis = 0)
-                entropy_msc = perfect_entropy(final_data, h = h, batch_size = batch_size)
-                diversity_msc = diversity(final_data, h=h, batch_size = batch_size)
-                return entropy_msc*np.log(diversity_msc)
-            
-            bounds = {'x': (0.1, 1)}
-            optimizer = BayesianOptimization(f=optimization_function, pbounds=bounds, random_state=1)
+                final_data = [frames[i] for i in indexes[: int(len(dset) * x)]]
+                final_data = np.concatenate(final_data, axis=0)
+                entropy_msc = perfect_entropy(final_data, h=h, batch_size=batch_size)
+                diversity_msc = diversity(final_data, h=h, batch_size=batch_size)
+                return entropy_msc * np.log(diversity_msc)
+
+            bounds = {"x": (0.1, 1)}
+            optimizer = BayesianOptimization(
+                f=optimization_function, pbounds=bounds, random_state=1
+            )
             optimizer.maximize(init_points=5, n_iter=20)
-            
-            return dset[indexes[:int(len(dset)*optimizer.max['params']['x'])]]
-    elif c_type == 'msc':
-        
+
+            return dset[indexes[: int(len(dset) * optimizer.max["params"]["x"])]]
+    elif c_type == "msc":
+
         # retrieve indexes
-        
-        indexes = minimum_set_coverage(frames, initial_entropies, h, l = 0)
-        
+
+        indexes = minimum_set_coverage(frames, initial_entropies, h, l=0)
+
         if compression_value != None:
-            return dset[indexes[:int(len(dset)*compression_value)]]
-        else:
-            # finding optimal compression value 
-            
-            def optimization_function(x):
-                final_data = [frames[i] for i in indexes[:int(len(dset)*x)]]
-                final_data = np.concatenate(final_data, axis = 0)
-                entropy_msc = perfect_entropy(final_data, h = h, batch_size = batch_size)
-                diversity_msc = diversity(final_data, h=h, batch_size = batch_size)
-                return entropy_msc*np.log(diversity_msc)
-            
-            bounds = {'x': (0.1, 1)}
-            optimizer = BayesianOptimization(f=optimization_function, pbounds=bounds, random_state=1)
-            optimizer.maximize(init_points=5, n_iter=20)
-            
-            return dset[indexes[:int(len(dset)*optimizer.max['params']['x'])]]
-    else:
-        if compression_value != None and l != None:
-            return dset[minimum_set_coverage(frames, initial_entropies, h, l)[:int(len(dset)*compression_value)]]
+            return dset[indexes[: int(len(dset) * compression_value)]]
         else:
             # finding optimal compression value
-            
+
+            def optimization_function(x):
+                final_data = [frames[i] for i in indexes[: int(len(dset) * x)]]
+                final_data = np.concatenate(final_data, axis=0)
+                entropy_msc = perfect_entropy(final_data, h=h, batch_size=batch_size)
+                diversity_msc = diversity(final_data, h=h, batch_size=batch_size)
+                return entropy_msc * np.log(diversity_msc)
+
+            bounds = {"x": (0.1, 1)}
+            optimizer = BayesianOptimization(
+                f=optimization_function, pbounds=bounds, random_state=1
+            )
+            optimizer.maximize(init_points=5, n_iter=20)
+
+            return dset[indexes[: int(len(dset) * optimizer.max["params"]["x"])]]
+    else:
+        if compression_value != None and l != None:
+            return dset[
+                minimum_set_coverage(frames, initial_entropies, h, l)[
+                    : int(len(dset) * compression_value)
+                ]
+            ]
+        else:
+            # finding optimal compression value
+
             def optimization_function(x, l):
                 indexes = minimum_set_coverage(frames, initial_entropies, h, l)
-                final_data = [frames[i] for i in indexes[:int(len(dset)*x)]]
-                final_data = np.concatenate(final_data, axis = 0)
-                entropy_msc = perfect_entropy(final_data, h = h, batch_size = batch_size)
-                diversity_msc = diversity(final_data, h=h, batch_size = batch_size)
-                return entropy_msc*np.log(diversity_msc)
-            
-            bounds = {'x': (0.1, 1), 'l': (0, 10)}
-            optimizer = BayesianOptimization(f=optimization_function, pbounds=bounds, random_state=1)
+                final_data = [frames[i] for i in indexes[: int(len(dset) * x)]]
+                final_data = np.concatenate(final_data, axis=0)
+                entropy_msc = perfect_entropy(final_data, h=h, batch_size=batch_size)
+                diversity_msc = diversity(final_data, h=h, batch_size=batch_size)
+                return entropy_msc * np.log(diversity_msc)
+
+            bounds = {"x": (0.1, 1), "l": (0, 10)}
+            optimizer = BayesianOptimization(
+                f=optimization_function, pbounds=bounds, random_state=1
+            )
             optimizer.maximize(init_points=5, n_iter=20)
-            
-            return dset[minimum_set_coverage(frames, initial_entropies, h, optimizer.max['params']['l'])
-                        [:int(len(dset)*optimizer.max['params']['x'])]]
-    
-def process_dataset(x: np.ndarray, initial_entropies: np.ndarray, num_chunks: int, num_sample: int, h, l):
+
+            return dset[
+                minimum_set_coverage(
+                    frames, initial_entropies, h, optimizer.max["params"]["l"]
+                )[: int(len(dset) * optimizer.max["params"]["x"])]
+            ]
+
+
+def process_dataset(
+    x: np.ndarray, initial_entropies: np.ndarray, num_chunks: int, num_sample: int, h, l
+):
     N = len(x)
 
     if N <= num_sample:
@@ -151,7 +179,12 @@ def process_dataset(x: np.ndarray, initial_entropies: np.ndarray, num_chunks: in
         start = i * chunk_size
         chunk = x[start : start + chunk_size]
         initial_entropies_chunk = initial_entropies[start : start + chunk_size]
-        y.append(start + np.array(minimum_set_coverage(chunk, initial_entropies_chunk, h, l, num_sample)))
+        y.append(
+            start
+            + np.array(
+                minimum_set_coverage(chunk, initial_entropies_chunk, h, l, num_sample)
+            )
+        )
 
     y = np.concatenate(y)
     result = []
@@ -160,13 +193,31 @@ def process_dataset(x: np.ndarray, initial_entropies: np.ndarray, num_chunks: in
     i = process_dataset(result, initial_entropies[y], num_chunks, num_sample, h, l)
     return y[i]
 
-def segment_compress(dset: List[Atoms], num_sample: int, num_chunks: int, k: int = DEFAULT_K, cutoff: float = DEFAULT_CUTOFF, h: float = DEFAULT_H, 
-                     batch_size: int = DEFAULT_BS, l: float = 0.0):
-    frames, initial_entropies = get_frame_descriptors(dset, k = k, cutoff = cutoff, h = h, batch_size = batch_size)
-    result = process_dataset(frames, initial_entropies, num_chunks = num_chunks, num_sample = num_sample, h = h, l = l)
+
+def segment_compress(
+    dset: List[Atoms],
+    num_sample: int,
+    num_chunks: int,
+    k: int = DEFAULT_K,
+    cutoff: float = DEFAULT_CUTOFF,
+    h: float = DEFAULT_H,
+    batch_size: int = DEFAULT_BS,
+    l: float = 0.0,
+):
+    frames, initial_entropies = get_frame_descriptors(
+        dset, k=k, cutoff=cutoff, h=h, batch_size=batch_size
+    )
+    result = process_dataset(
+        frames,
+        initial_entropies,
+        num_chunks=num_chunks,
+        num_sample=num_sample,
+        h=h,
+        l=l,
+    )
 
     final_dataset = []
-    
+
     for index in result:
         final_dataset.append(dset[index])
     return final_dataset
