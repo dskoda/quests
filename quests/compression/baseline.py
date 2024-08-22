783b42228a0a20fdd1342771d281c7e64185b0e0
unknown <yubenjamin2004@gmail.com>
2024-08-21 21:33:50 -0400

    DBSCan and k-means added

--- quests/compression/baseline.py
+++ quests/compression/baseline.py
@@ -22,10 +22,40 @@ def mean_fps(
     selected = fpsample.bucket_fps_kdtree_sampling(avg_descriptors, size)
     return selected
 
+def k_means(
+    descriptors: List[np.ndarray], entropies: np.ndarray, size: int
+) -> List[int]:
+    from sklearn.cluster import KMeans
+    
+    avg_descriptors = np.array([x.mean(0) for x in descriptors])
+    kmeans = KMeans(n_clusters=size, n_init="auto").fit(avg_descriptors)
+    selected = []
+    for num in range(size):
+        selected.append(np.random.choice(np.where(kmeans.labels_ == num)))
+    return selected        
 
 # TODO: Ben, please implement
 # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.DBSCAN.html#sklearn.cluster.DBSCAN
 def dbscan_sample(
     descriptors: List[np.ndarray], entropies: np.ndarray, size: int
 ) -> List[int]:
-    raise NotImplementedError
+    from sklearn.cluster import DBSCAN
+    from bayes_opt import BayesianOptimization 
+    
+    avg_descriptors = np.array([x.mean(0) for x in descriptors])
+    
+    def cost_fn(x):
+        dbscan = DBSCAN(eps = x, min_samples=1).fit(avg_descriptors)
+        return -abs(size-dbscan.labels_.max())
+    bounds = {'x': (0.0001, 0.05)}
+    
+    optimizer = BayesianOptimization(f=cost_fn, pbounds=bounds, random_state=1)
+    optimizer.maximize(init_points=5, n_iter=15)
+    
+    dbscan = DBSCAN(eps=optimizer.max['params']['x'], min_samples=1).fit(avg_descriptors)
+    
+    selected = []
+    for num in range(dbscan.labels_.max()):
+        selected.append(np.random.choice(np.where(dbscan.labels_ == num)))
+    return selected 
+    
