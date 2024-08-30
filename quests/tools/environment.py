a8d07c3a8dac942680d1162c6703594e32688a07
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2024-08-30 12:51:10 -0400

    fix: Update docstring formatting in environment.py

--- quests/tools/environment.py
+++ quests/tools/environment.py
@@ -11,15 +11,15 @@ def extract_environment(atoms: Atoms, idx: int, cutoff: float, k: int) -> Atoms:
 
     Arguments:
     ----------
-        atoms (Atoms): structure to be analyzed.
-        idx (int): index of the environment to be isolated.
-        cutoff (float): cutoff to consider when searching for
-            nearest neighbors.
-        k (int): number of nearest neighbors
+    atoms (Atoms): structure to be analyzed.
+    idx (int): index of the environment to be isolated.
+    cutoff (float): cutoff to consider when searching for
+        nearest neighbors.
+    k (int): number of nearest neighbors
 
     Returns:
     --------
-        env (Atoms): isolated environment of `atoms[idx]`
+    env (Atoms): isolated environment of `atoms[idx]`
     """
     i, j, d, D = neighbor_list("ijdD", atoms, cutoff=cutoff)
 
@@ -42,18 +42,18 @@ def extract_environment(atoms: Atoms, idx: int, cutoff: float, k: int) -> Atoms:
 
 def estimate_neighbors(density: float, cutoff: float, molar_mass: float):
     """Estimate how many neighbors are expected to be within
-        a given cutoff from the density of the material.
+    a given cutoff from the density of the material.
 
     Arguments:
     ----------
-        density (float): density of the material in g/cm3
-        cutoff (float): radius of the shell to be considered
-        molar_mass (float): molar mass of the material per formula unit
-            in g/mol.
+    density (float): density of the material in g/cm3
+    cutoff (float): radius of the shell to be considered
+    molar_mass (float): molar mass of the material per formula unit
+        in g/mol.
 
     Returns:
     --------
-        neighbors (float): average number of neighbors
+    neighbors (float): average number of neighbors
     """
     # 1 mol = 6.02E23 atoms
     # 1 cm^3 = 1E24 Å^3
