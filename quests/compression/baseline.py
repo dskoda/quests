efcac7e51a8acdb48bf85780656e8c7bd78edfdd
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2025-01-25 22:12:42 -0800

    k-means entropy as additional baseline

--- quests/compression/baseline.py
+++ quests/compression/baseline.py
@@ -24,7 +24,7 @@ def mean_fps(
 
     sampled_indices = [start_idx]
     min_distances = np.full(n_points, np.inf)
-    
+
     for _ in range(size - 1):
         current_point = avg_descriptors[sampled_indices[-1]]
         dist_to_current_point = np.linalg.norm(avg_descriptors - current_point, axis=1)
@@ -36,42 +36,66 @@ def mean_fps(
 
     return sampled_indices
 
+
 def k_means(
     descriptors: List[np.ndarray], entropies: np.ndarray, size: int
 ) -> List[int]:
     from sklearn.cluster import KMeans
-    
+
     avg_descriptors = np.array([x.mean(0) for x in descriptors])
-    kmeans = KMeans(n_clusters=size, n_init="auto").fit(avg_descriptors)
+    kmeans = KMeans(n_clusters=size).fit(avg_descriptors)
     selected = []
     for num in range(kmeans.labels_.max()):
         if sum(kmeans.labels_ == num) != 0:
             selected.append(np.random.choice(np.where(kmeans.labels_ == num)[0]))
-    return selected        
+    return selected
+
+
+def k_means_entropy(
+    descriptors: List[np.ndarray], entropies: np.ndarray, size: int
+) -> List[int]:
+    from sklearn.cluster import KMeans
+
+    avg_descriptors = np.array([x.mean(0) for x in descriptors])
+    kmeans = KMeans(n_clusters=size).fit(avg_descriptors)
+    selected = []
+    for num in range(kmeans.labels_.max()):
+        if sum(kmeans.labels_ == num) == 0:
+            continue
+
+        in_cluster = np.where(kmeans.labels_ == num)[0]
+        entr_cluster = entropies[in_cluster]
+        idx = in_cluster[entr_cluster.argmax()]
+        selected.append(idx)
+
+    return selected
+
 
 # TODO: Ben, please implement
 # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.DBSCAN.html#sklearn.cluster.DBSCAN
-def dbscan_sample(
-    descriptors, entropies: np.ndarray, size: int
-):
+def dbscan_sample(descriptors, entropies: np.ndarray, size: int):
     from sklearn.cluster import DBSCAN
-    from bayes_opt import BayesianOptimization 
-    
+    from bayes_opt import BayesianOptimization
+
     avg_descriptors = np.array([x.mean(0) for x in descriptors])
-    
+
     def cost_fn(x):
-        dbscan = DBSCAN(eps = x, min_samples=1).fit(avg_descriptors)
-        return -abs(size-dbscan.labels_.max())
-    bounds = {'x': (0.0001, 0.05)}
-    
-    optimizer = BayesianOptimization(f=cost_fn, pbounds=bounds, allow_duplicate_points=True)
+        dbscan = DBSCAN(eps=x, min_samples=1).fit(avg_descriptors)
+        return -abs(size - dbscan.labels_.max())
+
+    bounds = {"x": (0.0001, 0.05)}
+
+    optimizer = BayesianOptimization(
+        f=cost_fn, pbounds=bounds, allow_duplicate_points=True
+    )
     optimizer.set_gp_params
     optimizer.maximize(init_points=15, n_iter=15)
-    
-    dbscan = DBSCAN(eps=optimizer.max['params']['x'], min_samples=1).fit(avg_descriptors)
-    
+
+    dbscan = DBSCAN(eps=optimizer.max["params"]["x"], min_samples=1).fit(
+        avg_descriptors
+    )
+
     selected = []
     for num in range(dbscan.labels_.max()):
         selected.append(np.random.choice(np.where(dbscan.labels_ == num)[0]))
     return selected
-    
