74d00133e762bcac33180495bb4bdc351702d63b
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2025-04-23 09:46:46 -0700

    example on random multicomponent structures

--- quests/tools/example.py
+++ quests/tools/example.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 from ase.build import bulk, make_supercell
 
@@ -27,3 +28,25 @@ def get_noisy_structures(frac=1.0, noise=0.05, supercell_size=6):
     shcp.translate(noise * np.random.randn(len(shcp), 3))
 
     return sfcc, sbcc, shcp
+
+
+def get_multicomponent_structures(frac=1.0, supercell=1, ratio=0.5, seed: int = None):
+    fcc = bulk("Au", "fcc", a=frac * 4.08)
+    bcc = bulk("Au", "bcc", a=frac * 3.31)
+    hcp = bulk("Au", "hcp", a=frac * 2.88)
+
+    if supercell > 1:
+        fcc = make_supercell(fcc, np.eye(3) * supercell)
+        bcc = make_supercell(bcc, np.eye(3) * supercell)
+        hcp = make_supercell(hcp, np.eye(3) * supercell)
+
+    if seed is not None:
+        random.seed(seed)
+
+    for struct in [fcc, bcc, hcp]:
+        indices = list(range(len(struct)))
+        selected = random.sample(indices, k=int(ratio * len(struct)))
+        for i in selected:
+            struct[i].symbol = "Ag"
+
+    return fcc, bcc, hcp
